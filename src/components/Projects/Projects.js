89298a3cd8ef0dc2219d5936610146fbba508a80
Aadil Waghat <a.waghat16@gmail.com>
2023-05-19 12:56:58 +0100

    add ecommerce project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,7 @@ import "./Projects.css";
 import github from "../../assets/github.svg";
 import link from "../../assets/link.svg";
 import cricket from "../../assets/cricket.png";
+import ecommerce from '../../assets/ecommerce-screenshot.png'
 
 const Projects = () => {
   return (
@@ -26,6 +27,27 @@ const Projects = () => {
                 <img className="project-icon" src={github} alt="github-logo" />
               </a>
               <a href="https://cricketsearch-aadil.netlify.app/" target="_blank" rel="noreferrer">
+                <img className="project-icon" src={link} alt="link-icon" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="card ecommerce">
+          <img className="project-screenshot" src={ecommerce} alt="project-screenshot" />
+          <div className="project-wrapper-bg">
+            <h3 className="project-title">N&A Ecommerce</h3>
+            <br />
+            <h4 className="project-languages">HTML,CSS,JavaScript,React</h4>
+            <p className="project-desc">
+              Discover our trendy E-commerce Store, your ultimate destination for the latest Nike and Adidas clothing. Elevate your style with our diverse
+              collection, featuring sportswear essentials and fashionable athleisure. From sleek running shoes to performance-enhancing activewear, we offer a
+              range of high-quality options to suit your active lifestyle. Deployed with netlify
+            </p>
+            <div className="image-container">
+              <a href="https://github.com/a-waghat16/asosecommerce" target="_blank" rel="noreferrer">
+                <img className="project-icon" src={github} alt="github-logo" />
+              </a>
+              <a href="https://asosecommerce-aadil.netlify.app/" target="_blank" rel="noreferrer">
                 <img className="project-icon" src={link} alt="link-icon" />
               </a>
             </div>
